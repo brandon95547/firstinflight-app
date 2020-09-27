dbcdaa38c4e3747233b6c1bb292c459070c60782
Brandon Sanders <brandon95547@gmail.com>
2020-09-27 00:30:38 -0400

    sync

--- wp/wp-content/themes/twentynineteen/additions-functions.php
+++ wp/wp-content/themes/twentynineteen/additions-functions.php
@@ -10,5 +10,3 @@ wp_enqueue_style( 'material-icons', '/node-dev/node_modules/material-icons/iconf
 wp_enqueue_style( 'fontawesome', '/node-dev/node_modules/@fortawesome/fontawesome-free/css/all.min.css',false,'1.1','all');
 wp_enqueue_style( 'bootstrap', '/node-dev/node_modules/bootstrap/dist/css/bootstrap.min.css',false,'1.1','all');
 wp_enqueue_style( 'main', get_template_directory_uri() . '/css/app.min.css?v=' . time(),false,'1.1','all');
-
-// test
